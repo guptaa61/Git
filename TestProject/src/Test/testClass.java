6b442ae77b04c8b99d1360574609dbd73e942494
guptaa61 <ag3899@gmail.com>
2017-06-08 15:57:14 +0530

    test

--- TestProject/src/Test/testClass.java
+++ TestProject/src/Test/testClass.java
@@ -3,10 +3,7 @@ package Test;
 public abstract class testClass extends testConstructor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Java");
-
-		System.out.println("Everything done");
+	
 		testConstructor t1= new testConstructor();
 	
 		t1.add(20, 30);
