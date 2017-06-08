137afaa0d74bc1d75677dff8e56433cb102aab8d
guptaa61 <ag3899@gmail.com>
2017-06-08 15:51:14 +0530

    Added method

--- TestProject/src/Test/testConstructor.java
+++ TestProject/src/Test/testConstructor.java
@@ -10,6 +10,29 @@ public class testConstructor {
 		System.out.println("Addition is:" +c);
 		System.out.println("Constructor is running");
 	}
-	
+	public int add(int a , int b)
+	{
+		int c=a+b;
+		return c;
+		
+	}
+	public int substract(int a , int b)
+	{
+		int c=a-b;
+		return c;
+		
+	}
+	public int Multiply(int a , int b)
+	{
+		int c=a*b;
+		return c;
+		
+	}
+	public int Divide(int a , int b)
+	{
+		int c=a/b;
+		return c;
+		
+	}
 }
 
