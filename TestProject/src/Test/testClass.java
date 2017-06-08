c487ebe69481157b580bd34cf16a7b5293650d80
guptaa61 <ag3899@gmail.com>
2017-06-08 16:42:35 +0530

    sop entered

--- TestProject/src/Test/testClass.java
+++ TestProject/src/Test/testClass.java
@@ -1,14 +1,23 @@
 package Test;
 
-public abstract class testClass extends testConstructor {
+public class testClass extends testConstructor {
 
 	public static void main(String[] args) {
 	
 		testConstructor t1= new testConstructor();
 	
 		t1.add(20, 30);
+		System.out.println(t1.add(20, 30));
+		
 		t1.Divide(20,3);
+		System.out.println(t1.Divide(20,3));
+		
 		t1.substract(25, 52);
+		System.out.println(t1.substract(25, 52));
+		
+		t1.Multiply(10, 3);
+		System.out.println(t1.Multiply(10, 3));
+		
 		
 		
 		
