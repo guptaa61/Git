0e5093fed9d5dbb639399e5d39c69f15c9f59c47
guptaa61 <ag3899@gmail.com>
2017-06-08 15:58:36 +0530

    t

--- TestProject/src/Test/testConstructor.java
+++ TestProject/src/Test/testConstructor.java
@@ -4,11 +4,11 @@ public class testConstructor {
 	
 	public testConstructor() {
 		// TODO Auto-generated constructor stub
-		int a =10;
+		/*int a =10;
 		int b=20;
 		int c = a+b;
 		System.out.println("Addition is:" +c);
-		System.out.println("Constructor is running");
+		System.out.println("Constructor is running");*/
 	}
 	public int add(int a , int b)
 	{
